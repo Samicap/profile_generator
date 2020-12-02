4c9ce0ef0d44d0fc2e6184055d800fa2549145d5
Samicap <lovestochasewindmills@gmail.com>
2020-12-02 02:29:18 +0000

    finished survey.js

--- survery.js
+++ survery.js
@@ -9,20 +9,21 @@ const rl = readline.createInterface({
 rl.question('What is your name? Nicknames are also acceptable! ', (name) => {
   rl.question('What is your superpower? ', (superpower) => {
     rl.question("What's an activity you like doing? ", (activity) => {
-      rl.question("What do you listen to while doing that?", (listen) => {
-        rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (meal) => {
-          rl.question("What's your favourite thing to eat for that meal?", (favMeal) => {
-            rl.question("Which sport is your absolute favourite?", (sport) => {
-              console.log(`Hi there interglactic traveller!  I'm the alien ${name} from the planet XY-76345!  My superpower is ${superpower}! \n
-              While travelling at warp speed I like to listen to ${listen}.  Aboard my luxury space cruiser I like to eat ${favMeal} for ${meal}!  \n
-              Once I land on new planets I like to partake in a round of ${sport}!`);
+      rl.question("What do you listen to while doing that? ", (listen) => {
+        rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (meal) => {
+          rl.question("What's your favourite thing to eat for that meal? ", (favMeal) => {
+            rl.question("Which sport is your absolute favourite? ", (sport) => {
+              console.log(`Hi there interglactic traveller!  I'm the alien ${name} from the planet XY-76345!  My superpower is ${superpower}!
+              While travelling at warp speed I like to listen to ${listen}.  Aboard my luxury space cruiser I like to eat ${favMeal} for ${meal}!
+              Once I land on new planets I like to partake in a round of ${sport}! My favorite activity is ${activity}.`);
+              rl.close();
             });
           });
         });
       });
     });
   });
-  rl.close(); // terminates the session.
 });
+
 
 
